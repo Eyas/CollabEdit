334f726fa0d22eaf76cedfef6c0717bc92e08414
Eyas <eyas.sharaiha@gmail.com>
2014-11-26 01:13:36 -0500

    Finalizing TypeScript migration

---
+++ tsEdit/Scripts/typings/rangy/rangy-classapplier.d.ts
@@ -0,0 +1,19 @@
+﻿
+interface Rangy {
+    createCssClassApplier(
+        theclass: string,
+        options?: any,
+        tagNames?: string[]): ClassApplier;
+}
+
+interface ClassApplier {
+    applyToSelection(win?: Window): void;
+    undoToSelection(win?: Window): void;
+    isAppliedToSelection(win?: Window): boolean;
+    toggleSelection(win?: Window): void;
+
+    applyToRange(range: RangyRange): void;
+    undoToRange(range: RangyRange): void;
+    isAppliedToRange(range: RangyRange): boolean;
+    toggleRange(range: RangyRange): void;
+}
---
+++ tsEdit/Scripts/typings/rangy/rangy-core.d.ts
@@ -0,0 +1,49 @@
+﻿
+interface RangyConfig {
+}
+
+interface RangyDom {
+}
+
+interface RangyFeatures {
+}
+
+interface RangyModule {
+}
+
+interface RangyUtil {
+}
+
+interface RangyRange {
+}
+
+interface RangyPureJSRange {
+}
+
+interface RangySelection {
+}
+
+interface Rangy {
+    config: RangyConfig;
+    dom: RangyDom;
+    features: RangyFeatures;
+    initialized: boolean;
+    modules: RangyModule[];
+    supported: boolean;
+    util: RangyUtil;
+    DomRange(doc: Document): RangyRange;
+    Selection(selection: Selection): RangySelection;
+
+    addInitListener(listener: Function): void;
+    createMissingNativeApi(): void;
+    createNativeRange(doc?: Document): TextRange;
+    createRange(doc?: Document): RangyRange;
+    createRangyRange(doc?: Document): RangyPureJSRange;
+    getNativeSelection(win?: Window): Selection;
+    getSelection(win?: Window): RangySelection;
+    getIframeSelection(iframe: Element): RangySelection;
+
+    init(): void;
+
+    isSelectionValid(win?: Window): boolean;
+}
